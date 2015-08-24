7727385f9bbb39ac18ed2aa7512a604778820241
nikhilsu <nikhilsu@thoughtworks.com>
2015-08-24 17:52:19 +0530

    stage 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    
+    Scanner s = new Scanner(System.in);
+        System.out.print("Number of Inputs? ");
+        int n=s.nextInt();
+        String input[10];
+        int i=0;
+        input[i] = s.nextLine();
+
+
     }
 }
